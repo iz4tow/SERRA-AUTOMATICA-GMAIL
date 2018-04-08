12e37cd84867c1493405b0022c3d4a0931a2780d
franco <f.avino@melchioni.it>
2018-04-08 12:50:24 +0200

    08/04/2018 -

---
+++ gitB2B.bat
@@ -0,0 +1,7 @@
+git init
+git remote add origin https://iz4tow:Fiwoldiois01@github.com/iz4tow/SERRA-AUTOMATICA-GMAIL.git
+git add *
+set /p commento=INSERIRE COMMENTO PRIMA DELLA COMMIT: 
+git commit -m "%date% - %commento%"
+git push origin master
+pause > nul
